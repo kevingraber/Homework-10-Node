0d42da22c066bf9fb408dc9c7db0ea9bff89ddad
kg <kjg310@gmail.com>
2016-04-04 13:01:55 -0400

    Added Spotify Functionality

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 var Twitter = require('twitter');
 var keys = require('./keys.js')
+var spotify = require('spotify')
 
 var client = new Twitter({
 	consumer_key: keys.twitterKeys.consumer_key,
@@ -7,16 +8,68 @@ var client = new Twitter({
 	access_token_key: keys.twitterKeys.access_token_key,
 	access_token_secret: keys.twitterKeys.access_token_secret
 });
- 
-var params = {screen_name: 'kjg310'};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-  	if (!error) {
-	  	for (var i = 0; i < 20; i++) {
-	  		console.log("===== Tweet " + (i + 1) + " =====")
-	  		console.log(tweets[i].text);
-	    	console.log(tweets[i].created_at);
-		}
-  	}
-});
+
+
+
+var showTweets = function() {
+
+	var params = {screen_name: 'kjg310'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+	  	if (!error) {
+		  	for (var i = 0; i < 20; i++) {
+		  		console.log("===== Tweet " + (i + 1) + " =====")
+		  		console.log(tweets[i].text);
+		    	console.log(tweets[i].created_at);
+			}
+	  	}
+	});
+
+
+}
+
+var spotifyInfo = function(song) {
+	
+	spotify.search({type: 'track', query: song}, function(err, data) {
+		console.log(" ")
+		console.log("===== Spotify Search Results =====")
+		console.log(" ")
+		console.log("Song: " + data.tracks.items[0].name)
+		console.log("Album: " + data.tracks.items[0].album.name)
+		console.log("Artist: " + data.tracks.items[0].artists[0].name)
+		console.log("Preview URL: " + data.tracks.items[0].preview_url)
+		
+		
+	})
+
+}
+
+var movieInfo = function() {
+	console.log('Coming soon!')
+}
+
+switch(process.argv[2]) {
+	case 'my-tweets': 
+		showTweets();
+		break;
+	case 'spotify-this-song':
+		spotifyInfo(process.argv[3]);
+		break;
+	case 'movie-this':
+		movieInfo();
+		break;
+	default:
+		console.log("I'm sorry Dave, I'm afraid I can't do that..");
+}
+
+// var params = {screen_name: 'kjg310'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response){
+//   	if (!error) {
+// 	  	for (var i = 0; i < 20; i++) {
+// 	  		console.log("===== Tweet " + (i + 1) + " =====")
+// 	  		console.log(tweets[i].text);
+// 	    	console.log(tweets[i].created_at);
+// 		}
+//   	}
+// });
 
 // console.log(keys)
